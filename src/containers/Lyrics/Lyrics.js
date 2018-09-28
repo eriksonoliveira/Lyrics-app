dd1dc2cc241c651831f9218e19a0b41c9ede6a46
Erikson Oliveira <eriksonmagno6@hotmail.com>
2018-09-28 05:19:32 -0400

    fix key

--- src/containers/Lyrics/Lyrics.js
+++ src/containers/Lyrics/Lyrics.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { createURLArtist } from "../../spotify";
-import key from "../../keys/keys_dev";
-// import key from "../../keys/keys";
+// import key from "../../keys/keys_dev";
+import key from "../../keys/keys";
 
 import "./Lyrics.css";
 
