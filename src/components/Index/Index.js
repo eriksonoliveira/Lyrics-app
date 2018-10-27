42a5c5feb2a2bd518b22ab6639e7be0cbf1d3a69
Erikson Oliveira <eriksonmagno6@hotmail.com>
2018-10-27 02:38:47 -0400

    refactor state management 2

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -1,27 +1,25 @@
 import React from "react";
-import { TrackProvider, TrackConsumer } from "../../TrackContext";
+import { TrackConsumer } from "../../TrackContext";
 import { AppConsumer } from "../../AppContext";
 import Tracks from "../Track/Tracks";
 import Search from "../../containers/Search/Search";
 
 const Index = props => {
   return (
-    <TrackProvider>
-      <TrackConsumer>
-        {({ trackTitle, list_active, tracks_list, heading }) => (
-          <AppConsumer>
-            {({ dispatch }) => (
-              <React.Fragment>
-                <Search trackTitle={trackTitle} appDispatch={dispatch} />
-                {list_active && (
-                  <Tracks heading={heading} tracks_list={tracks_list} />
-                )}
-              </React.Fragment>
-            )}
-          </AppConsumer>
-        )}
-      </TrackConsumer>
-    </TrackProvider>
+    <TrackConsumer>
+      {({ trackTitle, list_active, tracks_list, heading }) => (
+        <AppConsumer>
+          {({ dispatch }) => (
+            <React.Fragment>
+              <Search trackTitle={trackTitle} appDispatch={dispatch} />
+              {list_active && (
+                <Tracks heading={heading} tracks_list={tracks_list} />
+              )}
+            </React.Fragment>
+          )}
+        </AppConsumer>
+      )}
+    </TrackConsumer>
   );
 };
 
