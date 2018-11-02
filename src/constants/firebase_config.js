98c056db17a05a386d71d0e451a63ca1cbd096ee
Erikson Oliveira <eriksonmagno6@hotmail.com>
2018-11-02 13:11:10 -0400

    fix IE problem with firebase

---
+++ src/constants/firebase_config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyD0rJ5MGF6_IjHgV4xJXWuUwZ7KKxf_2ME",
+  authDomain: "lyrics-finder-861fb.firebaseapp.com",
+  databaseURL: "https://lyrics-finder-861fb.firebaseio.com",
+  projectId: "lyrics-finder-861fb",
+  storageBucket: "lyrics-finder-861fb.appspot.com",
+  messagingSenderId: "952185169443"
+};
